434eedd57e4b57d7280b7d4827e0523397acd805
Waquar <admin@waquar.com>
2025-07-25 01:05:20 +0530

    minor updates

--- next.config.ts
+++ next.config.ts
@@ -5,3 +5,9 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+module.exports = withBundleAnalyzer({})
